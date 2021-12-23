6879867e4a0411f138d8d9c06172e4f8260fedcb
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2021-12-23 10:54:08 -0500

    Edit movie object
    change structure
    add image paths
    add descriptions

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,12 +8,27 @@ export class MainView extends React.Component {
         super();
         this.state = {
             movies: [
-                { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-                { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-                { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
+                { 
+                    _id: 1, 
+                    Title: 'Inception', 
+                    Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', 
+                    ImagePath: 'https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392/?ref_=ext_shr_lnk'
+                },
+                { 
+                    _id: 2, 
+                    Title: 'The Shawshank Redemption', 
+                    Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 
+                    ImagePath: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=ext_shr_lnk'
+                },
+                { 
+                    _id: 3, 
+                    Title: 'Gladiator', 
+                    Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', 
+                    ImagePath: 'https://www.imdb.com/title/tt0172495/mediaviewer/rm2442542592/?ref_=ext_shr_lnk'
+                }
             ],
             selectedMovie = null
-        }
+        };
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -30,15 +45,11 @@ export class MainView extends React.Component {
         return (
             <div className="main-view">
                 {selectedMovie
-                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { 
-                        this.setSelectedMovie(newSelectedMovie); }}/>
-                    : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { 
-                        this.setSelectedMovie(movie) }}/>
-                    ))
+                     ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                     : movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />)
                 }
             </div>
         );
-      }
+    }
 
 }
