8b4a3ed1e292218db2dce43940312ae3b186ef8d
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2021-12-23 14:19:14 -0500

    dist update

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -22753,22 +22753,22 @@ class MainView extends _reactDefault.default.Component {
                     _id: 1,
                     Title: 'Inception',
                     Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
-                    ImagePath: 'https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392/?ref_=ext_shr_lnk'
+                    ImagePath: '../../../img/inception.jpg'
                 },
                 {
                     _id: 2,
                     Title: 'The Shawshank Redemption',
                     Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-                    ImagePath: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=ext_shr_lnk'
+                    ImagePath: '../../../img/shawshank.jpg'
                 },
                 {
                     _id: 3,
                     Title: 'Gladiator',
                     Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',
-                    ImagePath: 'https://www.imdb.com/title/tt0172495/mediaviewer/rm2442542592/?ref_=ext_shr_lnk'
+                    ImagePath: '../../../img/gladiator.jpg'
                 }
             ],
-            selectedMovie=null
+            selectedMovie: null
         };
     }
     setSelectedMovie(newSelectedMovie) {
