97d4ab5e605699f749364fbe7d376620c27b7d6a
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2021-12-28 11:17:19 -0500

    Import axios

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -8,27 +10,39 @@ export class MainView extends React.Component {
         super();
         this.state = {
             movies: [
-                { 
-                    _id: 1, 
-                    Title: 'Inception', 
-                    Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', 
-                    ImagePath: '../../../img/inception'
-                },
-                { 
-                    _id: 2, 
-                    Title: 'The Shawshank Redemption', 
-                    Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 
-                    ImagePath: '../../../img/shawshank'
-                },
-                { 
-                    _id: 3, 
-                    Title: 'Gladiator', 
-                    Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', 
-                    ImagePath: '../../../img/gladiator'
-                }
+                // { 
+                //     _id: 1, 
+                //     Title: 'Inception', 
+                //     Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', 
+                //     ImagePath: '../../../img/inception'
+                // },
+                // { 
+                //     _id: 2, 
+                //     Title: 'The Shawshank Redemption', 
+                //     Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 
+                //     ImagePath: '../../../img/shawshank'
+                // },
+                // { 
+                //     _id: 3, 
+                //     Title: 'Gladiator', 
+                //     Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', 
+                //     ImagePath: '../../../img/gladiator'
+                // }
             ],
             selectedMovie: null
         };
+    }
+
+    componentDidMount(){
+        axios.get('https://blooming-wildwood-80599.herokuapp.com/movies')
+            .then(response => {
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(error => {
+                console.log(error);
+        });
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -40,8 +54,8 @@ export class MainView extends React.Component {
     render() {
         const { movies, selectedMovie } = this.state;
       
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-      
+        if (movies.length === 0) return <div className="main-view" />;
+              
         return (
             <div className="main-view">
                 {selectedMovie
