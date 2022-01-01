aa55d947070c8736566bc93e8fdf89ad77ee60ae
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2022-01-01 14:51:31 -0500

    Responsive Bootstrap

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
@@ -58,12 +60,22 @@ export class MainView extends React.Component {
         if (movies.length === 0) return <div className="main-view" />;
 
         return (
-            <div className="main-view">
+            <Row className="main-view justify-content-md-center">
                 {selectedMovie
-                     ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                     : movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />)
+                    ? (
+                    <Col md={8}>
+                        <MovieView movie={selectedMovie} 
+                        onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                    </Col>
+                    )
+                    : movies.map(movie => (
+                    <Col md={3}>
+                        <MovieCard key={movie._id} movie={movie} 
+                        onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+                    </Col>
+                    ))
                 }
-            </div>
+            </Row>
         );
     }
 
