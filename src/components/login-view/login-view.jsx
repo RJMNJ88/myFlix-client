9186620e6f5b9e08c6cfdd07adda0513e43e15d2
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2022-01-04 13:37:37 -0500

    Login
    Authentication

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,16 +3,26 @@ import { Card, CardGroup, Container, Row, Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+import axios from 'axios';
+
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
     /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
+    axios.post('https://blooming-wildwood-80599.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+    .then(response => {
+      const data = response.data;
+      props.onLoggedIn(data);
+    })
+    .catch(e => {
+      console.log('User could not be found.')
+    });
   };
 
   return (
