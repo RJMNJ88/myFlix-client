d386a07e34d305d559d75192b6ea9a63d50dd809
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2022-01-07 16:39:58 -0500

    Create director view

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { Button, Container, Card, Row, Col } from 'react-bootstrap';
+
+import "./director-view.css";
+
+export class DirectorView extends React.Component {
+  render () {
+    const {Director, onBackClick } = this.props;
+
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Img className='director-img' variant='top' src={Director.ImagePath} crossOrigin='anonymous' />
+                <div className="name">
+                  <span className="label">Name: </span>
+                  <span className="value">{Director.Name}</span>
+                </div>
+                <div className="born">
+                  <span className="label">Born: </span>
+                  <span className="value">{Director.Born}</span>
+                </div>
+                <div className="bio">
+                  <span className="label">Bio: </span>
+                  <span className="value">{Director.Bio}</span>
+                </div>
+                <div className="films">
+                  <span className="label">Popular Films: </span>
+                  <span className="value">{Director.PopularFilms}</span>
+                </div>
+                <div className="backButton">
+                  <Button size="md" variant="outline-primary" onClick={() => { onBackClick(null); }}>Back</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        
+      </Container>
+    );
+  }
+}
+
+DirectorView.PropTypes = {
+  Director: PropTypes.shape({
+    Name: PropTypes.string,
+    Born: PropTypes.string,
+    Bio: PropTypes.string,
+    PopularFilms: PropTypes.array
+    // PopularFilms: PropTypes.arrayOf(PropTypes.string)
+  })
+}
